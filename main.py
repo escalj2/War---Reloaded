1f53e6bbae03635ff84ba124ee4c90b99ab9f1c8
Jordan Escalante <escalj2@gmail.com>
2025-12-04 19:30:29 -0800

    added a faceoff function, where when there is a tie, each side puts 3 cards into the pot, and then they put down another card, whichever is higher gets the whole pot

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ ui.label('Hello NiceGUI!')
 ui.run()"""
 
 game = True
+tie = False
 
 deck1 = [
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
@@ -23,7 +24,75 @@ deck2 = [
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
             ]
 
+def face_off(tie, deck1, deck2, pot1, pot2):
+    while tie == True:
+        for x in range(0, 3):
+            card1 = random.choice(deck1)
+            card2 = random.choice(deck2)
+
+            deck1.remove(card1)
+            deck2.remove(card2)
+
+            pot1.append(card1)
+            pot2.append(card2)
+
+        print("")
+        print("Pot 1: ", pot1)
+        print("Pot 2: ", pot2)
+        time.sleep(3)
+
+        card1 = random.choice(deck1)
+        card2 = random.choice(deck2)
+
+        deck1.remove(card1)
+        deck2.remove(card2)
+
+        print("")
+        print("And the winner is...")
+        time.sleep(3)
+        print("Player 1: ", card1)
+        print("Player 2: ", card2)
+
+        if card1 > card2:
+            print("Player 1!")
+            deck1.append(card1)
+            deck1.append(card2)
+            deck1.extend(pot1)
+            deck1.extend(pot2)
+            print("Deck 1: \n", deck1)
+            print("Deck 1: \n", deck2)
+            print("Press any key to continue: ")
+            x = input()
+            tie = False
+            pot1.clear(), pot2.clear()
+            return tie, deck1, deck2, pot1, pot2
+
+        elif card2 > card1:
+            print("Player 2!")
+            deck2.append(card1)
+            deck2.append(card2)
+            deck2.extend(pot1)
+            deck2.extend(pot2)
+            print("Deck 1: \n", deck1)
+            print("Deck 1: \n", deck2)
+            print("Press any key to continue: ")
+            x = input()
+            time.sleep(5)
+            tie = False
+            pot1.clear(), pot2.clear()
+            return tie, deck1, deck2, pot1, pot2
+
 while game == True:
+
+    #Win condition
+    """if len(deck1) or len(deck2) == 0:
+        game == False
+        print("Game over!")
+        time.sleep(1)
+        break """
+    
+    pot1 = []
+    pot2 = []
 
     card1 = random.choice(deck1)
     card2 = random.choice(deck2)
@@ -31,7 +100,7 @@ while game == True:
     deck1.remove(card1)
     deck2.remove(card2)
 
-    #os.system('cls')
+    os.system('cls')
     print("Cards in deck 1: ", len(deck1))
     print("Cards in deck 1: ", len(deck2))
 
@@ -52,10 +121,18 @@ while game == True:
         deck2.append(card1)
         deck2.append(card2)
     else:
-        print("Tie!")
+        print("Tie! Time to face off!")
+        tie = True
+        pot1.append(card1)
+        pot2.append(card2)
+        print(deck1)
+        print(deck2)
+        print("Press any key to continue: ")
+        x = input()
+        face_off(tie, deck1, deck2, pot1, pot2)
 
     time.sleep(1)
-    #os.system('cls')
+    os.system('cls')
 
     print("Cards in deck 1: ", len(deck1))
     print("Cards in deck 2: ", len(deck2))
@@ -63,3 +140,5 @@ while game == True:
     print(deck1)
     print(deck2)
     print("")
+
+
