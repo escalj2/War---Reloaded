ac26605361d138e611c2761027f63c004c3aeffe
Jordan Escalante <escalj2@gmail.com>
2025-12-08 19:24:42 -0800

    rewrote code to use more functions, making it more clear, including draw_cards function, print_message function, and decide_winner function. Also added continue statement for face_off so multiple face-offs can happen in a row, but need to test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import time
 import os
+
 """from nicegui import ui
 
 ui.label('Hello NiceGUI!')
@@ -28,15 +29,50 @@ deck2 = [
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
             ]
 
+def draw_cards(deck1, deck2):
+    card1 = random.choice(deck1)
+    card2 = random.choice(deck2)
+
+    deck1.remove(card1)
+    deck2.remove(card2)
+    os.system('cls')
+
+    return card1, card2
+
+def print_message(card1, card2, deck1, deck2):
+    print("Cards in deck 1: ", len(deck1))
+    print("Cards in deck 2: ", len(deck2))
+    print("Drawing cards...\n")
+    time.sleep(1)
+    print("Player 1: ", card1)
+    print("Player 2: ", card2)
+    print("")
+    time.sleep(1)
+
+def decide_winner(card1, card2, deck1, deck2):
+    if card1 > card2:
+        print("Player 1 wins!")
+        deck1.append(card1)
+        deck1.append(card2)
+    elif card2 > card1:
+        print("Player 2 wins!")
+        deck2.append(card1)
+        deck2.append(card2)
+    else:
+        print("Tie! Time to face off!")
+        tie = True
+        pot1.append(card1)
+        pot2.append(card2)
+        print(deck1)
+        print(deck2)
+        print("Press any key to continue: ")
+        x = input()
+        face_off(tie, deck1, deck2, pot1, pot2)
+
 def face_off(tie, deck1, deck2, pot1, pot2):
     while tie == True:
         for x in range(0, 3):
-            card1 = random.choice(deck1)
-            card2 = random.choice(deck2)
-
-            deck1.remove(card1)
-            deck2.remove(card2)
-
+            card1, card2 = draw_cards(deck1, deck2)
             pot1.append(card1)
             pot2.append(card2)
 
@@ -45,11 +81,7 @@ def face_off(tie, deck1, deck2, pot1, pot2):
         print("Pot 2: ", pot2)
         time.sleep(3)
 
-        card1 = random.choice(deck1)
-        card2 = random.choice(deck2)
-
-        deck1.remove(card1)
-        deck2.remove(card2)
+        card1, card2 = draw_cards(deck1, deck2)
 
         print("")
         print("And the winner is...")
@@ -85,10 +117,14 @@ def face_off(tie, deck1, deck2, pot1, pot2):
             tie = False
             pot1.clear(), pot2.clear()
             return tie, deck1, deck2, pot1, pot2
+        
+        else:
+            continue
 
 while game == True:
 
-    #Win condition
+    # Check win condition
+
     """if len(deck1) or len(deck2) == 0:
         game == False
         print("Game over!")
@@ -98,42 +134,11 @@ while game == True:
     pot1 = []
     pot2 = []
 
-    card1 = random.choice(deck1)
-    card2 = random.choice(deck2)
+    card1, card2 = draw_cards(deck1, deck2)
+    print_message(card1, card2, deck1, deck2)
+    decide_winner(card1, card2, deck1, deck2)
 
-    deck1.remove(card1)
-    deck2.remove(card2)
-
-    os.system('cls')
-    print("Cards in deck 1: ", len(deck1))
-    print("Cards in deck 1: ", len(deck2))
-
-    print("Drawing cards...\n")
-    time.sleep(1)
-
-    print("Player 1: ", card1)
-    print("Player 2: ", card2)
-    print("")
-    time.sleep(1)
-
-    if card1 > card2:
-        print("Player 1 wins!")
-        deck1.append(card1)
-        deck1.append(card2)
-    elif card2 > card1:
-        print("Player 2 wins!")
-        deck2.append(card1)
-        deck2.append(card2)
-    else:
-        print("Tie! Time to face off!")
-        tie = True
-        pot1.append(card1)
-        pot2.append(card2)
-        print(deck1)
-        print(deck2)
-        print("Press any key to continue: ")
-        x = input()
-        face_off(tie, deck1, deck2, pot1, pot2)
+    
 
     time.sleep(1)
     os.system('cls')
